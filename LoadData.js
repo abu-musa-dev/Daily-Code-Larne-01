3e5a83fcd4cdf700b7746fa6e5518bea03036d4f
Md Abu Musa <abumusa0740@gmail.com>
2026-06-10 07:23:17 +0600

    add function

--- LoadData.js
+++ LoadData.js
@@ -25,6 +25,21 @@
 // LoadData()
 
 
+// console.log("connected js folder ")
+//  async function LoadData() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//    const data = await response.json()
+//    const user = document.getElementById("users")
+//      data.forEach
+
+// }
+
+// LoadData()
+
+
+// console.log(data)
+
+
 console.log("connected js folder ")
  async function LoadData() {
    const response = await fetch("https://jsonplaceholder.typicode.com/users")
