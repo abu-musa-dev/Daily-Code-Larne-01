e494c0aec8fc6191dccbba5980df503c91098050
Md Abu Musa <abumusa0740@gmail.com>
2026-06-12 10:30:33 +0600

    Meth in js

---
+++ Module-01-JS-Basics/Math.js
@@ -0,0 +1,9 @@
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+console.log(totalPrice)
+console.log(DiscountPrice)
