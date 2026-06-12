00fc1844d1bf8e7f966cbc0b9a266a9838942b06
Md Abu Musa <abumusa0740@gmail.com>
2026-06-12 09:21:06 +0600

    add level

--- LoadData.js
+++ LoadData.js
@@ -113,6 +113,92 @@ console.log(data)
 // console.log(data)
 
 
+// console.log("connected js folder ")
+//  async function LoadData() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//    const data = await response.json()
+//    const user = document.getElementById("users")
+//      data.forEach
+
+// }
+
+// LoadData()
+
+
+
+async function LoadData() {
+    const data = await fetch('/data.the')
+    const response = response.json()
+    data 
+        
+}
+
+
+
+console.log(data)
+
+
+console.log("connected js folder ")
+ async function LoadData() {
+   const response = await fetch("https://jsonplaceholder.typicode.com/users")
+   const data = await response.json()
+   const user = document.getElementById("users")
+     data.forEach
+
+}
+
+LoadData()
+
+
+console.log(data)
+
+console.log(data)
+console.log(data)
+console.log(data)
+console.log(data)
+
+// const response = await fetch(url);
+// const data = await response.json();
+// console.log(data);
+
+// const response = wait fetch(url);
+// const data = wait response.sjon()
+// console.log(data)
+
+// async function LoadData() {
+
+//      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//      const data = await response.json()
+//      console.log(data)
+// } 
+
+// LoadData()
+
+
+// async function LoadData (){
+//      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//      const data  = await response.json()
+//      console.log(data)
+// }
+
+// LoadData()
+
+
+// console.log("connected js folder ")
+//  async function LoadData() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//    const data = await response.json()
+//    const user = document.getElementById("users")
+//      data.forEach
+
+// }
+
+// LoadData()
+
+
+// console.log(data)
+
+
 console.log("connected js folder ")
  async function LoadData() {
    const response = await fetch("https://jsonplaceholder.typicode.com/users")
@@ -140,4 +226,92 @@ console.log("connected js folder ")
 LoadData()
 
 
+console.log(data)
+
+
+
+// const response = await fetch(url);
+// const data = await response.json();
+// console.log(data);
+
+// const response = wait fetch(url);
+// const data = wait response.sjon()
+// console.log(data)
+
+// async function LoadData() {
+
+//      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//      const data = await response.json()
+//      console.log(data)
+// } 
+
+// LoadData()
+
+
+// async function LoadData (){
+//      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//      const data  = await response.json()
+//      console.log(data)
+// }
+
+// LoadData()
+
+
+// console.log("connected js folder ")
+//  async function LoadData() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//    const data = await response.json()
+//    const user = document.getElementById("users")
+//      data.forEach
+
+// }
+
+// LoadData()
+
+
+// console.log(data)
+
+
+// console.log("connected js folder ")
+//  async function LoadData() {
+//    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//    const data = await response.json()
+//    const user = document.getElementById("users")
+//      data.forEach
+
+// }
+
+// LoadData()
+
+
+
+async function LoadData() {
+    const data = await fetch('/data.the')
+    const response = response.json()
+    data 
+        
+}
+
+
+
+console.log(data)
+
+
+console.log("connected js folder ")
+ async function LoadData() {
+   const response = await fetch("https://jsonplaceholder.typicode.com/users")
+   const data = await response.json()
+   const user = document.getElementById("users")
+     data.forEach
+
+}
+
+LoadData()
+
+
+console.log(data)
+
+console.log(data)
+console.log(data)
+console.log(data)
 console.log(data)
