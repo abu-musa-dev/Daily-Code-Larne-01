43ed9914e008f7dcd16bbf11560ee1892c33e129
Md Abu Musa <abumusa0740@gmail.com>
2026-06-12 12:29:35 +0600

    add dom monipulation dom click event add

---
+++ Module-05-DOM-Manipulation/day1-dom-select.js
@@ -0,0 +1,47 @@
+// const Mytitle = document.getElementById("main-title")
+// const MyTitleTwo = document.querySelector(".description")
+
+// Mytitle.textContent = "i have a plan"
+// MyTitleTwo.textContent = "No It IS cird dada"
+
+// Mytitle.style.color = "red"
+// MyTitleTwo.style.color= "blue"
+
+// console.log(Mytitle)
+// console.log(MyTitleTwo)
+
+
+//style er vitor gyea joto hula proparti ace sob gula bosiea dite parbo style dia 
+//and text content thika text change korte parbo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const MyBtn= document.getElementById("action-btn")
+console.log(MyBtn)
+const myHeading = document.getElementById("main-title")
+const description = document.getElementById("description")
+
+
+MyBtn.addEventListener("click", ()=>{
+    myHeading.textContent = "thsa hurry my code is runn"
+    description.textContent = "lorem this is lorem text in here the cotent out off the text"
+    myHeading.style.color ="red"
+
+    MyBtn.style.color ="red"
+})
