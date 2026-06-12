65af232f02a7b74217db4aeb5fc2ae624119630a
Md Abu Musa <abumusa0740@gmail.com>
2026-06-12 10:48:02 +0600

    conditinal rendaring

---
+++ Module-02-Conditionals-and-Loops/Module-02-Conditionals-and-Loops.js
@@ -0,0 +1,14 @@
+// let clientBudget = 500;
+// let ProductPrice = 500;
+
+// if(clientBudget > ProductPrice) {
+//     console.log("Product ti kinte paren congratulation")
+// }
+
+// else if (clientBudget == ProductPrice){
+//     console.log("apnar budget soman nite paren product congratulation")
+// }
+
+// else {
+//     console.log("apni gorib apnar taka nai produti nibar taka churi koren ")
+// }
