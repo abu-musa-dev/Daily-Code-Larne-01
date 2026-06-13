92a75ebbc9da2005c962f6def6f9193956fef9e3
Md Abu Musa <abumusa0740@gmail.com>
2026-06-13 07:22:08 +0600

    explorer id and query selector

--- Module-05-DOM-Manipulation/day1-dom-select.js
+++ Module-05-DOM-Manipulation/day1-dom-select.js
@@ -32,16 +32,32 @@
 
 
 
-const MyBtn= document.getElementById("action-btn")
-console.log(MyBtn)
-const myHeading = document.getElementById("main-title")
-const description = document.getElementById("description")
+// const MyBtn= document.getElementById("action-btn")
+// console.log(MyBtn)
+// const myHeading = document.getElementById("main-title")
+// const description = document.getElementById("description")
 
 
-MyBtn.addEventListener("click", ()=>{
-    myHeading.textContent = "thsa hurry my code is runn"
-    description.textContent = "lorem this is lorem text in here the cotent out off the text"
-    myHeading.style.color ="red"
+// MyBtn.addEventListener("click", ()=>{
+//     myHeading.textContent = "thsa hurry my code is runn"
+//     description.textContent = "lorem this is lorem text in here the cotent out off the text"
+//     myHeading.style.color ="red"
 
-    MyBtn.style.color ="red"
-})
+//     MyBtn.style.color ="red"
+// })
+
+
+
+const array1 = ["komla", "appple", "litchu"]
+const array2 = ["kodu", "karpet", "kakroll"]
+
+// const allarray = [...array1, ...array2]
+
+// console.log(allarray)
+
+
+
+const baseClient = {
+    name: "Jetsy",
+    platform: "Kajabi"
+};
