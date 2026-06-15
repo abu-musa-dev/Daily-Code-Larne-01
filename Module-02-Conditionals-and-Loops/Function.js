9dc1fa181be04bd6bde36c88ef05d23d7b580bd3
Md Abu Musa <abumusa0740@gmail.com>
2026-06-15 08:12:41 +0600

    calculator funcinility add

--- Module-02-Conditionals-and-Loops/Function.js
+++ Module-02-Conditionals-and-Loops/Function.js
@@ -1,40 +1,34 @@
-function Musa(age){
-    return "age" + age;
-}
+// function Musa(age){
+//     return "age" + age;
+// }
 
-const result = Musa("25")
+// const result = Musa("25")
 
-console.log(result)
-
-
-const MulTiply = (x , y) => x * y 
+// console.log(result)
 
 
-
-let arry = [ "apple", "komla", "'lebu", "sugar" ]
-arry.pop()
-
-console.log(arry)
-
-let client = {
-    name: "Jetsy Ruiz",
-    platform: "Kajabi",
-    experienceYears: 11,
-    isHappy: true
-};
+// const MulTiply = (x , y) => x * y 
 
 
-client.name = "Jefa"
-client.experienceYears = 21
-client.platform = "kajabi"
 
-console.log(client)
+// let arry = [ "apple", "komla", "'lebu", "sugar" ]
+// arry.pop()
 
-client.name = "Jefa"
-client.experienceYears = 21
-client.platform = "kajabi"
+// console.log(arry)
 
-console.log(client)
+// let client = {
+//     name: "Jetsy Ruiz",
+//     platform: "Kajabi",
+//     experienceYears: 11,
+//     isHappy: true
+// };
+
+
+// client.name = "Jefa"
+// client.experienceYears = 21
+// client.platform = "kajabi"
+
+// console.log(client)
 
 
 
@@ -44,10 +38,11 @@ console.log(client)
 
 
 
-let packages = ["Free Consult", "Strategic Session", "Ongoing Coaching"];
+
+// let packages = ["Free Consult", "Strategic Session", "Ongoing Coaching"];
 
 
-packages.forEach((pkg, index)=>{
-    console.log(pkg + (index+1))
+// packages.forEach((pkg, index)=>{
+//     console.log(pkg + (index+1))
 
-})
+// })
--- Module-02-Conditionals-and-Loops/Module-02-Conditionals-and-Loops.js
+++ Module-02-Conditionals-and-Loops/Module-02-Conditionals-and-Loops.js
@@ -1,31 +1,31 @@
-// let clientBudget = 500;
-// let ProductPrice = 500;
+let clientBudget = 500;
+let ProductPrice = 500;
 
-// if(clientBudget > ProductPrice) {
-//     console.log("Product ti kinte paren congratulation")
-// }
+if(clientBudget > ProductPrice) {
+    console.log("Product ti kinte paren congratulation")
+}
 
-// else if (clientBudget == ProductPrice){
-//     console.log("apnar budget soman nite paren product congratulation")
-// }
+else if (clientBudget == ProductPrice){
+    console.log("apnar budget soman nite paren product congratulation")
+}
 
-// else {
-//     console.log("apni gorib apnar taka nai produti nibar taka churi koren ")
-// }
-
-
-
-// important loop
-
-// for(let number=1; number <= 10; number++ ){
-
-//     console.log(number)
-// }
+else {
+    console.log("apni gorib apnar taka nai produti nibar taka churi koren ")
+}
 
 
 
+important loop
+
+for(let number=1; number <= 10; number++ ){
+
+    console.log(number)
+}
 
 
-// for(let number = 1; number <= 10; number ++ ){
-//     console.log(number)
-// }
+
+
+
+for(let number = 1; number <= 10; number ++ ){
+    console.log(number)
+}
