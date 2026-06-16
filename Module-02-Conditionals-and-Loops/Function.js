d8c7db560f89b030b9d6617bdb582505045274b0
Md Abu Musa <abumusa0740@gmail.com>
2026-06-16 08:03:02 +0600

    Update Ui

--- Module-02-Conditionals-and-Loops/Function.js
+++ Module-02-Conditionals-and-Loops/Function.js
@@ -1,13 +1,13 @@
-function Musa(age){
-    return "age" + age;
-}
+// function Musa(age){
+//     return "age" + age;
+// }
 
-const result = Musa("25")
+// const result = Musa("25")
 
-console.log(result)
+// console.log(result)
 
 
-const MulTiply = (x , y) => x * y 
+// const MulTiply = (x , y) => x * y 
 
 
 
@@ -16,22 +16,19 @@ arry.pop()
 
 console.log(arry)
 
-let client = {
-    name: "Jetsy Ruiz",
-    platform: "Kajabi",
-    experienceYears: 11,
-    isHappy: true
-};
+// let client = {
+//     name: "Jetsy Ruiz",
+//     platform: "Kajabi",
+//     experienceYears: 11,
+//     isHappy: true
+// };
 
 
-client.name = "Jefa"
-client.experienceYears = 21
-client.platform = "kajabi"
+// client.name = "Jefa"
+// client.experienceYears = 21
+// client.platform = "kajabi"
 
-console.log(client)
-
-
-
+// console.log(client)
 
 
 
@@ -39,10 +36,13 @@ console.log(client)
 
 
 
-let packages = ["Free Consult", "Strategic Session", "Ongoing Coaching"];
 
 
-packages.forEach((pkg, index)=>{
-    console.log(pkg + (index+1))
 
-})
+// let packages = ["Free Consult", "Strategic Session", "Ongoing Coaching"];
+
+
+// packages.forEach((pkg, index)=>{
+//     console.log(pkg + (index+1))
+
+// })
