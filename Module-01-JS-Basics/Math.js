f9fc6d4877d9f31ac2a169d21ac705bd7f170fb0
Md Abu Musa <abumusa0740@gmail.com>
2026-06-16 08:04:09 +0600

    update data

--- Module-01-JS-Basics/Math.js
+++ Module-01-JS-Basics/Math.js
@@ -73,4 +73,46 @@ const String = "30";
 // console.log( Number == String ) //True  // check only valu 
 // console.log( Number == String ) //False  // check valu and data types
 
+const Number = 30 ;
+const String = "30";
 
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
