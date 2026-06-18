6e144dcf272e5e70e4861df1504257de3c0e463f
Md Abu Musa <abumusa0740@gmail.com>
2026-06-18 08:39:23 +0600

    data logic build

--- Module-01-JS-Basics/add.js
+++ Module-01-JS-Basics/add.js
@@ -336,3 +336,21 @@ const DiscountPrice = totalPrice - 10 ;
 
 const Number = 30 ;
 const String = "30";
+
+
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
