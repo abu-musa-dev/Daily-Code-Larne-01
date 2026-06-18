a653e0a7598108a7cfd0535ccb435abf02323ae3
Md Abu Musa <abumusa0740@gmail.com>
2026-06-18 09:17:44 +0600

    update alowe

--- Module-01-JS-Basics/add.js
+++ Module-01-JS-Basics/add.js
@@ -29,19 +29,19 @@ const DiscountPrice = totalPrice - 10 ;
 
 
 
-const Number = 30 ;
-const String = "30";
+// const Number = 30 ;
+// const String = "30";
 
-console.log( Number == String ) //True  // check only valu 
-console.log( Number == String ) //False  // check valu and data types
-
-
-const FirstPrice = 500
-const SecondPrice = 100
+// console.log( Number == String ) //True  // check only valu 
+// console.log( Number == String ) //False  // check valu and data types
 
 
-const totalPrice = FirstPrice + SecondPrice;
-const DiscountPrice = totalPrice - 10 ;
+// const FirstPrice = 500
+// const SecondPrice = 100
+
+
+// const totalPrice = FirstPrice + SecondPrice;
+// const DiscountPrice = totalPrice - 10 ;
 
 // console.log(totalPrice)
 // console.log(DiscountPrice)
