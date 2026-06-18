b04a80904913494fa2998410ab13b23c88511e83
Md Abu Musa <abumusa0740@gmail.com>
2026-06-18 08:38:26 +0600

    update here data

--- Module-01-JS-Basics/add.js
+++ Module-01-JS-Basics/add.js
@@ -157,3 +157,164 @@ const DiscountPrice = totalPrice - 10 ;
 
 const Number = 30 ;
 const String = "30";
+
+
+// const FirstPrice = 500
+// const SecondPrice = 100
+
+
+// const totalPrice = FirstPrice + SecondPrice;
+// const DiscountPrice = totalPrice - 10 ;
+
+// // console.log(totalPrice)
+// // console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+// console.log( Number == String ) //True  // check only valu 
+// console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+// console.log( Number == String ) //True  // check only valu 
+// console.log( Number == String ) //False  // check valu and data types
+
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
+const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";const Number = 30 ;
+const String = "30";
+
+console.log( Number == String ) //True  // check only valu 
+console.log( Number == String ) //False  // check valu and data types
+
+
+const FirstPrice = 500
+const SecondPrice = 100
+
+
+const totalPrice = FirstPrice + SecondPrice;
+const DiscountPrice = totalPrice - 10 ;
+
+// console.log(totalPrice)
+// console.log(DiscountPrice)
+
+
+
+const Number = 30 ;
+const String = "30";
