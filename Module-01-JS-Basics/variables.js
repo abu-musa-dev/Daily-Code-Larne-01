9a9bbe320660f4b1ea98002849ff1737effd7141
Md Abu Musa <abumusa0740@gmail.com>
2026-06-20 16:49:23 +0600

    new features add

--- Module-01-JS-Basics/variables.js
+++ Module-01-JS-Basics/variables.js
@@ -18,3 +18,12 @@ let arry = [ "apple", "komla", "'lebu", "sugar" ]
 arry.pop()
 
 console.log(arry)
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
