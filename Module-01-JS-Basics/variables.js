917915e9b839b0cd892aee7f03d7c833856b757e
Md Abu Musa <abumusa0740@gmail.com>
2026-06-21 09:35:21 +0600

    Optimize application performance

--- Module-01-JS-Basics/variables.js
+++ Module-01-JS-Basics/variables.js
@@ -12,3 +12,36 @@ console.log(arry)
 
 
 // console.log(arry)
+
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
+let arry = [ "apple", "komla", "'lebu", "sugar" ]
+arry.pop()
+
+console.log(arry)
+
