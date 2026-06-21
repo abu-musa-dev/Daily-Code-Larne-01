31fed5f3e08a6e800bbb870e1faf8e12f967c73e
Md Abu Musa <abumusa0740@gmail.com>
2026-06-21 09:34:58 +0600

    Add local storage support

--- Module-01-JS-Basics/variables.js
+++ Module-01-JS-Basics/variables.js
@@ -3,12 +3,12 @@ arry.pop()
 
 console.log(arry)
 
-let arry = [ "apple", "komla", "'lebu", "sugar" ]
-arry.pop()
+// let arry = [ "apple", "komla", "'lebu", "sugar" ]
+// arry.pop()
 
-console.log(arry)
-
-
+// console.log(arry)
 
 
-console.log(arry)
+
+
+// console.log(arry)
