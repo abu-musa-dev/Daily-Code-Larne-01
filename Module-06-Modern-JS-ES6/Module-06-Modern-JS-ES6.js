c16003dc9bca1cde272461061cb33c2eab7728ea
Md Abu Musa <abumusa0740@gmail.com>
2026-06-23 10:58:27 +0600

    add even number checker

--- Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
+++ Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
@@ -11,3 +11,9 @@ const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map(num => num * 2);
 
 console.log(doubled);
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+console.log(isEven(8));
