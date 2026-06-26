ad28022594f0aa75a3707532f4422a290523ff12
Md Abu Musa <abumusa0740@gmail.com>
2026-06-26 16:31:49 +0600

    Calculate array sum

--- Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
+++ Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
@@ -51,3 +51,13 @@ if (word === reverse) {
 } else {
   console.log("Not Palindrome");
 }
+
+let numbers = [10, 20, 30, 40];
+
+let sum = 0;
+
+for (let num of numbers) {
+  sum += num;
+}
+
+console.log(sum);
