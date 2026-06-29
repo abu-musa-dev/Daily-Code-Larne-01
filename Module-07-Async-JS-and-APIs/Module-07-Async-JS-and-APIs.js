a3a71023c890b88395fae585224ddcb3961040e5
Md Abu Musa <abumusa0740@gmail.com>
2026-06-29 08:12:59 +0600

    Remove duplicate values from array

--- Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
+++ Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
@@ -202,3 +202,9 @@ let numbers = [1, 2, 2, 3, 4, 4, 5];
 let uniqueNumbers = [...new Set(numbers)];
 
 console.log(uniqueNumbers);
+
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+
+let uniqueNumbers = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
