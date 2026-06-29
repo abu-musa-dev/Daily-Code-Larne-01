d50bd19a0cca8f6c0150f00fb6175defba4f3eeb
Md Abu Musa <abumusa0740@gmail.com>
2026-06-29 08:12:28 +0600

    Check palindrome string

--- Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
+++ Module-07-Async-JS-and-APIs/Module-07-Async-JS-and-APIs.js
@@ -186,3 +186,13 @@ for (let num of numbers) {
 }
 
 console.log(sum);
+
+let word = "madam";
+
+let reversed = word.split("").reverse().join("");
+
+if (word === reversed) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
