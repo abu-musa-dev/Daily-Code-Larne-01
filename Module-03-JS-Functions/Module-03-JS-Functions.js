1116b424a3fd6c6d6a882f752be818230659fd9e
Md Abu Musa <abumusa0740@gmail.com>
2026-07-08 09:43:18 +0600

    Add largest number comparison

--- Module-03-JS-Functions/Module-03-JS-Functions.js
+++ Module-03-JS-Functions/Module-03-JS-Functions.js
@@ -79,4 +79,11 @@ if (number % 2 === 0) {
     console.log("Even");
 } else {
     console.log("Odd");
+}let a = 30;
+let b = 45;
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
 }
