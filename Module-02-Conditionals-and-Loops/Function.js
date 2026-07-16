ee117bc66a04e3ee8de98cd134e973aa9052d031
Md Abu Musa <abumusa0740@gmail.com>
2026-07-16 15:12:43 +0600

    feat: add string reversal function

--- Module-02-Conditionals-and-Loops/Function.js
+++ Module-02-Conditionals-and-Loops/Function.js
@@ -11,3 +11,16 @@ function checkEvenOdd(num) {
   return num % 2 === 0 ? "Even" : "Odd";
 }
 console.log(checkEvenOdd(7)); // Output: Odd
+
+git add .
+git commit -m "feat: add array sum calculation function"
+git push// reverse-string.js
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("hello")); // Output: olleh
+
+
+git add reverse-string.js
+git commit -m "feat: add string reversal function"
+git push
