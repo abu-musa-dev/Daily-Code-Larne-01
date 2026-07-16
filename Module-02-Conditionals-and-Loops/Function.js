53c43a3bd99e40cbf00ebcb64485e7c9cb505d75
Md Abu Musa <abumusa0740@gmail.com>
2026-07-16 15:11:23 +0600

    feat: add even-odd number checker

--- Module-02-Conditionals-and-Loops/Function.js
+++ Module-02-Conditionals-and-Loops/Function.js
@@ -1,48 +1,5 @@
-// function Musa(age){
-//     return "age" + age;
-// }
-
-// const result = Musa("25")
-
-// console.log(result)
-
-
-// const MulTiply = (x , y) => x * y 
-
-
-
-let arry = [ "apple", "komla", "'lebu", "sugar" ]
-arry.pop()
-
-console.log(arry)
-
-// let client = {
-//     name: "Jetsy Ruiz",
-//     platform: "Kajabi",
-//     experienceYears: 11,
-//     isHappy: true
-// };
-
-
-// client.name = "Jefa"
-// client.experienceYears = 21
-// client.platform = "kajabi"
-
-// console.log(client)
-
-
-
-
-
-
-
-
-
-
-// let packages = ["Free Consult", "Strategic Session", "Ongoing Coaching"];
-
-
-// packages.forEach((pkg, index)=>{
-//     console.log(pkg + (index+1))
-
-// })
+// even-odd.js
+function checkEvenOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+console.log(checkEvenOdd(7)); // Output: Odd
