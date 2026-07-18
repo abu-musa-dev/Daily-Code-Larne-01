67b90dfdfdab142f417d0256e17c6fd3260a3c01
Md Abu Musa <abumusa0740@gmail.com>
2026-07-18 10:26:44 +0600

    Add recursive factorial calculation function

--- Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
+++ Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
@@ -269,4 +269,8 @@ console.log(findLargestNum([10, 5, 20, 8])); // Output: 20function isPalindrome(
     const reversed = str.split('').reverse().join('');
     return str === reversed;
 }
-console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("madam")); // Output: truefunction factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
