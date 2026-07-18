964859e1bb32bf03ab713f19d90d9f33a4fcbcad
Md Abu Musa <abumusa0740@gmail.com>
2026-07-18 10:27:11 +0600

    Add solution to remove duplicate elements from array

--- Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
+++ Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
@@ -273,4 +273,7 @@ console.log(isPalindrome("madam")); // Output: truefunction factorial(n) {
     if (n === 0 || n === 1) return 1;
     return n * factorial(n - 1);
 }
-console.log(factorial(5)); // Output: 120
+console.log(factorial(5)); // Output: 120function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
