33819e39f8a00a91fb46c2ea19f31fea055a4f5b
Md Abu Musa <abumusa0740@gmail.com>
2026-07-18 10:28:29 +0600

    Add Celsius to Fahrenheit temperature converter

--- Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
+++ Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
@@ -294,4 +294,7 @@ console.log(countVowels("javascript")); // Output: 3function sumArray(arr) {
 console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15function isEven(num) {
     return num % 2 === 0;
 }
-console.log(isEven(7)); // Output: false
+console.log(isEven(7)); // Output: falsefunction celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+console.log(celsiusToFahrenheit(30)); // Output: 86
