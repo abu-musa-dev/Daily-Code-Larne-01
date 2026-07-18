a226dbe715066af50efbbe810520858b2e9500ff
Md Abu Musa <abumusa0740@gmail.com>
2026-07-18 10:27:27 +0600

    Add classic FizzBuzz algorithm solution

--- Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
+++ Module-06-Modern-JS-ES6/Module-06-Modern-JS-ES6.js
@@ -276,4 +276,12 @@ console.log(isPalindrome("madam")); // Output: truefunction factorial(n) {
 console.log(factorial(5)); // Output: 120function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
+}
+fizzBuzz(15);
