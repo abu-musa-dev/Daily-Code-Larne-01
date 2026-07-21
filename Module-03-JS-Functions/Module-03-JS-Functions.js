0c19705d155b2ad2170e1b2047294f352fbc2478
Md Abu Musa <abumusa0740@gmail.com>
2026-07-21 11:30:36 +0600

    Calculate sum of array elements

--- Module-03-JS-Functions/Module-03-JS-Functions.js
+++ Module-03-JS-Functions/Module-03-JS-Functions.js
@@ -16,4 +16,11 @@ if (a > b) {
   console.log(b);for (let i = 1; i <= 10; i++) {
   console.log(`5 x ${i} = ${5 * i}`);
 }
+}let numbers = [10, 20, 30, 40];
+let sum = 0;
+
+for (let num of numbers) {
+  sum += num;
 }
+
+console.log(sum);
