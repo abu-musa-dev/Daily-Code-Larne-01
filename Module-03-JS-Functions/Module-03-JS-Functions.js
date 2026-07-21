a6c9cbbdadd4df7c86f65285255c3e3459c95963
Md Abu Musa <abumusa0740@gmail.com>
2026-07-21 11:29:54 +0600

    Create even and odd number checker

--- Module-03-JS-Functions/Module-03-JS-Functions.js
+++ Module-03-JS-Functions/Module-03-JS-Functions.js
@@ -1,4 +1,10 @@
 console.log("Hello, JavaScript!");let a = 10;
 let b = 20;
 
-console.log(a + b);
+console.log(a + b);let num = 15;
+
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
