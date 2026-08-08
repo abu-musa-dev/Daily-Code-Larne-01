6f809da397cab6e02509db603efb0bf05b9bcf70
Md Abu Musa <abumusa0740@gmail.com>
2026-08-08 08:16:07 +0600

    Flatten nested array using JavaScript

--- Module-05-DOM-Manipulation/day1-dom-select.js
+++ Module-05-DOM-Manipulation/day1-dom-select.js
@@ -187,3 +187,10 @@ Problem:
 
 Problem:
 দুটি string anagram কিনা check করো।
+10. Flatten a Nested Array
+
+Problem:
+একটি nested array-কে single array-তে convert করো।
+
+// Input: [1,[2,[3,4]],5]
+// Output: [1,2,3,4,5]
